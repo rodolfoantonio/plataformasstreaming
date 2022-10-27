f70bc822d0bdfa78a122bf56e47ec7ef84655621
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-10-27 16:27:00 -0500

    icon person fill

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Navbar.css";
 import Buscar from "../buscar/Buscar";
+import Userlogout from "../userlogout/Userlogout";
+
 
 const Navbar = () => {
   return (
@@ -34,35 +36,9 @@ const Navbar = () => {
               <Buscar />
             </li>
           </ul>
-          <div className="dropdown dropstart userIcon">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="30"
-                fill="white"
-                className="bi bi-person"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-              </svg>  
-            </button>
 
-            <ul className="dropdown-menu dropdown-menu-dark bg-dark" aria-labelledby="dropdownMenuButton1">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Log out
-                </a>
-              </li>
-            </ul>
-
-          </div>
+          <Userlogout/>
+          
         </div>
       </div>
     </nav>
---
+++ src/components/userlogout/Userlogout.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Userlogout.css";
+
+const Userlogout = () => {
+  return (
+    <div className="dropdown dropstart userIcon">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="white"
+          className="bi bi-person-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+        </svg>
+      </button>
+
+      <ul
+        className="dropdown-menu dropdown-menu-dark bg-dark"
+        aria-labelledby="dropdownMenuButton1"
+      >
+        <li>
+          <a className="dropdown-item" href="#">
+            Log out
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Userlogout;
