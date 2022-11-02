a9283f8046fbdb00d97f39759da22b5154618e4c
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-11-02 07:37:29 -0500

    se cambio el nombre del archivo App.js a App2.js para evitar conflictos con el redux

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import Footer from './components/footer/Footer'; 
 
 import App from './App';
-import { Provider } from 'react-redux';
-import  store  from "./redux/store";
+//import { Provider } from 'react-redux';
+//import  store  from "./redux/store";
 
 
 //import reportWebVitals from './reportWebVitals';
