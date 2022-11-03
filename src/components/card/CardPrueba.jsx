cbadb0a8ad95fa14ce819c691fc3f1b926c26dbb
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-11-02 20:27:41 -0500

    se agrega el compoente de alquilar

--- src/components/card/CardPrueba.jsx
+++ src/components/card/CardPrueba.jsx
@@ -3,13 +3,14 @@ import { URL_IMAGE } from "../../api/apiConfig";
 
 const CardPrueba = ({ movie }) => {
   return (
-    <div key={movie.id} className="col-md-4 mb-2 border">
+    <div key={movie.id} className="col-md-4 mb-2 border position-relative">
       <img
         src={`${URL_IMAGE + movie.poster_path}`}
         alt=""
         height={400}
         width="100%"
       />
+      <button className="btn-danger position-absolute end-0 top-0 m-2">Alquilar</button>
       <div className="px-3 pt-2">
         <h2 className="pt-3">{`${movie.title} (${movie.release_date})`}</h2>
         <h5>Idioma: {movie.original_language}</h5>
