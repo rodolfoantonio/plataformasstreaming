0ddfddf6345e1a5e8bb422380e95518f6c76201e
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-11-02 19:40:29 -0500

    se agrego la card dde ivan a la cardprueba se puso la ruta url_img a apiCongi.js

--- src/components/card/CardPrueba.jsx
+++ src/components/card/CardPrueba.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { URL_IMAGE } from "../../api/apiConfig";
 
 const CardPrueba = ({ movie }) => {
   return (
-    <div className="col-sm-4" key={movie.id + "_id"}>
-      <div className="card text-white bg-dark m-1">
-        <div className="card-body">
-          <h5 className="card-title">{movie.original_title}</h5>
-          <p className="card-text">{movie.overview}</p>
-        </div>
+    <div key={movie.id} className="col-md-4 mb-2 border">
+      <img
+        src={`${URL_IMAGE + movie.poster_path}`}
+        alt=""
+        height={400}
+        width="100%"
+      />
+      <div className="px-3 pt-2">
+        <h2 className="pt-3">{`${movie.title} (${movie.release_date})`}</h2>
+        <h5>Idioma: {movie.original_language}</h5>
+        <h5>Calificación: {`${movie.vote_average} / 10`}</h5>
+        <p className="text-justify">
+          {" "}
+          <b style={{ fontSize: 18 }}>Descripción:</b> {movie.overview}
+        </p>
       </div>
     </div>
   );
